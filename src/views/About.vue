768615a86141e85d76711838607504aa8d053d20
cchura94 <cchura.cpc@gmail.com>
2020-05-08 18:45:55 -0400

    commit inicial

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Acerca de Nosotros</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, nobis, voluptates nisi veniam qui minus rerum quasi velit assumenda alias quae porro eos asperiores quos, quis possimus veritatis. Modi, a?</p>
+
+    <h2>Misión</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil assumenda fuga reiciendis, eaque maxime accusamus culpa? Mollitia nam fugiat ratione animi repudiandae porro et molestias. Laudantium velit totam similique necessitatibus!Aliquid eum, rerum cupiditate quae officia autem in quo deleniti amet nisi, nobis culpa doloribus et, necessitatibus asperiores porro facere inventore excepturi consequatur distinctio. Delectus sapiente temporibus id error quo.</p>
+
+    <h2>Visión</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quo autem non a, facilis pariatur itaque maiores aliquam culpa atque nobis soluta eum eligendi reiciendis. Neque explicabo dolorum porro iure.
+    Assumenda doloribus libero soluta tenetur eveniet voluptatem corporis dolorum est, molestiae nemo dignissimos, praesentium consequatur delectus voluptate labore quod laboriosam iste atque ipsa similique dolorem fuga ad aliquam! Quas, neque!
+    Optio commodi est tempora laborum sint illum quas fugiat dicta distinctio, nesciunt modi odit dolor perspiciatis nihil maiores ut atque excepturi! Porro amet ratione aut ad, nesciunt molestiae neque corporis.</p>
+
+
   </div>
 </template>
---
+++ src/views/Contacto.vue
@@ -0,0 +1,27 @@
+<template>
+    <dir>
+        <h1>Contactenos</h1>
+        
+        <label for="">Email</label>
+        <br>
+        <input type="text">
+        <br>
+        <label for="">Mensaje:</label>
+        <br>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <br>
+        <button>Enviar mensaje</button>
+        <button type="reset">Restablecer</button>
+
+    </dir>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
